cf27a817e3c6a6e307498fe695493a9b2a3e4758
Andrew Kuo <andrewkuo@Emmas-MacBook-Pro.local>
2020-08-02 21:38:49 +1000

    new flask code

--- ai_api/application.py
+++ ai_api/application.py
@@ -1,21 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+import json
 import tensorflow as tf
 import gpt_2_simple as gpt2
 
-sess = gpt2.start_tf_sess()
-gpt2.load_gpt2(sess, run_name='run2')
-graph = tf.get_default_graph()
+class GptModel:
+  def __init__(self):
+    self.sess = gpt2.start_tf_sess()
+    gpt2.load_gpt2(self.sess, run_name='run1')
+    self.graph = tf.get_default_graph()
 
+  def predict(self, prefix):
+    print('pred working')
+    with self.graph.as_default():
+      txt = gpt2.generate(self.sess, run_name='run1', length=200, prefix=prefix, return_as_list=True)[0]
+    return txt
+
+gptmodel = GptModel()
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/v1/gpt2', methods=['GET'])
+@app.route('/v1/gpt2', methods=['POST'])
 def gpt2_model():
-    with graph.as_default():
-        txt = gpt2.generate(sess, run_name='run2', length=200, return_as_list=True)[0]
-    return txt
+  print('req received')
+  data = json.loads(request.data.decode("utf-8"))
+  txt = gptmodel.predict(data['text'])
+  return txt
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+  app.debug = True
+  app.run(threaded=False)
